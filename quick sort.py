300013147cba21f7db79d23a10a1d8a353dfd7b6
Allen An <zifeng.allen@gmail.com>
2020-01-01 14:15:53 -0500

    update quick sort

--- quick sort.py
+++ quick sort.py
@@ -1,32 +1,15 @@
-def quick_sort(nums):
-   if len(nums) <= 1:
-       return nums
-   else:
-       mid = nums[0]
-       nums1, nums2 = [], []
-       for n in nums[1:]:
-           if n <= mid:
-               nums1.append(n)
-           else:
-               nums2.append(n)
+def qs(l:list):
+    if len(l)<=1:
+        return l
+    else:
+        pivot=l[0]
+        left=[]
+        right=[]
+        for num in l[1:]:
+            if num>=pivot:
+                right.append(num)
+            else:
+                left.append(num)
+        return qs(left)+[pivot]+qs(right)
 
-       print(nums1, nums2)
-       return combine(append_to_end(quick_sort(nums1), mid), quick_sort(nums2))
-
-
-def combine(list1, list2):
-   if list2 == None:
-       return list1
-   for i in list2:
-       append_to_end(list1, i)
-   return list1
-
-
-def append_to_end(list, obj):
-   if obj == None:
-       return list
-   list.append(obj)
-   return list
-
-
-print(quick_sort([5, 4, 8, 6, 7, 9, 1, 2, 5]))
+print(qs([5, 4, 8, 6, 7, 9, 1, 2, 5]))
